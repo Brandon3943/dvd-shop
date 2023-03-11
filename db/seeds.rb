84733d5e57e878c21da8965c5f8c4f02710216a8
root <root@Ty-Laptop.localdomain>
2023-03-11 14:01:12 -0500

    completed assignment

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create(title: "Haryr Potter", year: 2012, length: 130, director: "idk", description: "magicccc", category: "fansty", discount: true, female_director: true)
